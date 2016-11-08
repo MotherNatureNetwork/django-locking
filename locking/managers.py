4771ec249b338595db0b279a862dd2bf5d73cbf1
Justin Caratzas <bigjust@lambdaphil.es>
2016-11-08 16:28:31 -0500

    get_query_set -> get_queryset

--- locking/managers.py
+++ locking/managers.py
@@ -7,8 +7,8 @@ import datetime
 """
     LOCKED
             if (datetime.today() - self.locked_at).seconds < LOCK_TIMEOUT:
-            
-            
+
+
             self.locked_at < (NOW - TIMEOUT)
 """
 
@@ -17,11 +17,11 @@ def point_of_timeout():
     return timezone.now() - delta
 
 class LockedManager(Manager):
-    def get_query_set(self):
+    def get_queryset(self):
         timeout = point_of_timeout()
         return super(LockedManager, self).get_query_set().filter(_locked_at__gt=timeout, _locked_at__isnull=False)
 
 class UnlockedManager(Manager):
-    def get_query_set(self):
+    def get_queryset(self):
         timeout = point_of_timeout()
         return super(UnlockedManager, self).get_query_set().filter(Q(_locked_at__lte=timeout) | Q(_locked_at__isnull=True))
